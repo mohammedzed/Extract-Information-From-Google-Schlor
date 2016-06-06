4d5280222ff9a72b78c96f2f05c671b99dbc48f7
mohammedzed <mohammedzulqarnain@yahoo.com>
2016-06-06 11:25:48 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Extract-Information-From-Google-Schlor
+
+Rerunning the Scraper
+---------------------
+
+Assuming you have [node.js](http://nodejs.org/) installed, rerun the scraper as follows:
+
+```
+$ npm install request cheerio async
+$ node scrape.js ./people-hci.json > stats-hci.js
+```
+
+To scrape the images:
+
+```
+$ node download-images.js ./stats-hci.js
+```
+
+Then open up `index.html` and it should display the new statistics.
---
+++ stats-hci.js
@@ -0,0 +1,53 @@
+var date = "Mon Jun 06 2016 11:17:40 GMT+0300 (Arab Standard Time)"
+var data = [
+  {
+    "name": "Ashraf Aboulnaga",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=47sihrsAAAAJ&citpid=3",
+    "keywords": [
+      "Databases",
+      "Cloud computing"
+    ],
+    "affiliation": "Qatar Computing Research Institute",
+    "url": "https://scholar.google.com/citations?user=47sihrsAAAAJ&hl=en&oi=sra",
+    "stats": {
+      "citations": [
+        "2636",
+        "1328"
+      ],
+      "hindex": [
+        "27",
+        "22"
+      ],
+      "i10index": [
+        "42",
+        "39"
+      ]
+    },
+    "year": "2000"
+  },
+  {
+    "name": "Gregory D. Abowd",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=gb8sbdcAAAAJ&citpid=1",
+    "keywords": [
+      "Human-Computer Interaction",
+      "Mobile and Ubiquitous Computing"
+    ],
+    "affiliation": "Regents' Professor and J.Z. Liang Chair, School of Interactive Computing, Georgia Institute of",
+    "url": "http://scholar.google.com/citations?user=gb8sbdcAAAAJ",
+    "stats": {
+      "citations": [
+        "37035",
+        "12573"
+      ],
+      "hindex": [
+        "69",
+        "45"
+      ],
+      "i10index": [
+        "215",
+        "145"
+      ]
+    },
+    "year": "1995"
+  }
+];
